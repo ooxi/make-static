fd87bb4f10ae17d16739c7054997a48b27c2c9b4
ooxi <violetland@mail.ru>
2022-11-28 20:41:07 +0100

    Remove tailing whitespace

--- src/Document.js
+++ src/Document.js
@@ -2,23 +2,23 @@
  * Copyright (c) 2014 github/ooxi
  *     https://github.com/ooxi/make-static
  *     violetland@mail.ru
- * 
+ *
  * This software is provided 'as-is', without any express or implied warranty.
  * In no event will the authors be held liable for any damages arising from the
  * use of this software.
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
+ *
  *  1. The origin of this software must not be misrepresented; you must not
  *     claim that you wrote the original software. If you use this software in a
  *     product, an acknowledgment in the product documentation would be
  *     appreciated but is not required.
- * 
+ *
  *  2. Altered source versions must be plainly marked as such, and must not be
  *     misrepresented as being the original software.
- * 
+ *
  *  3. This notice may not be removed or altered from any source distribution.
  */
 var async = require('async');
@@ -39,7 +39,7 @@ var ReferencedDocuments = require('./ReferencedDocuments.js');
 /**
  * Will download one document into the local file system while changing
  * references to assets and other documents.
- * 
+ *
  * @param {URI} _base Base URI below which assets and documents should be
  *     resolved
  * @param {Asset} _asset Used to get asset paths
@@ -49,7 +49,7 @@ var ReferencedDocuments = require('./ReferencedDocuments.js');
  *     `directory')
  */
 module.exports = function(_base, _asset, _path, _directory) {
-	
+
 	/**
 	 * Directory has to end with a slash and must exist
 	 */
@@ -57,23 +57,23 @@ module.exports = function(_base, _asset, _path, _directory) {
 		? _directory
 		: (_directory + '/');
 	mkdirp.sync(_directory);
-	
-	
-	
-	
-	
+
+
+
+
+
 	/**
 	 * @param {URI} uri Location of document to download (basic checks
-	 *     are already done 
+	 *     are already done
 	 * @param {callback} cb Should lead to `modify'
 	 */
 	var download = function(uri, cb) {
-		
+
 		/* Request document
 		 */
 		request(uri.toString(), function(err, response) {
 			var HTTP_OK = 200;
-			
+
 			if (err) {
 				cb(err);
 				return;
@@ -82,17 +82,17 @@ module.exports = function(_base, _asset, _path, _directory) {
 				cb(new Error('Unexpected response status code `'+ response.statusCode +'\' expected `'+ HTTP_OK +'\''));
 				return;
 			}
-			
+
 			cb(null, uri, response);
 		});
 	};
-	
-	
+
+
 
 	/**
 	 * Will resolve references to assets and save references to other
 	 * documents
-	 * 
+	 *
 	 * @param {URI} uri Document's location
 	 * @param {http.ServerResponse} response The document already
 	 *     downloaded from the server
@@ -100,8 +100,8 @@ module.exports = function(_base, _asset, _path, _directory) {
 	 */
 	var modifyHtml = function(uri, response, cb) {
 		var $ = cheerio.load(response.body);
-		
-		
+
+
 		/* Collect assets
 		 */
 		var referenced_assets = new ReferencedAssets(
@@ -110,16 +110,16 @@ module.exports = function(_base, _asset, _path, _directory) {
 		$('img[src]').each(new referenced_assets('src'));
 		$('script').each(new referenced_assets('src'));
 		$('link[rel="stylesheet"]').each(new referenced_assets('href'));
-		
-		
+
+
 		/* Collect document references
 		 */
 		var reference_document = new ReferencedDocuments(
 			_base, uri, _path
 		);
 		$('a[href]').each(new reference_document('href'));
-		
-		
+
+
 		/* Download all assets
 		 */
 		async.parallelLimit(referenced_assets.callbacks, 10, function(err) {
@@ -127,7 +127,7 @@ module.exports = function(_base, _asset, _path, _directory) {
 				cb(err);
 				return;
 			}
-			
+
 			/* Serialize DOM to HTML
 			 */
 			cb(null, uri, 'text/html', $.html(), reference_document.references);
@@ -138,7 +138,7 @@ module.exports = function(_base, _asset, _path, _directory) {
 
 	/**
 	 * Will not perform any modifications on binary documents.
-	 * 
+	 *
 	 * @param {URI} uri Document's location
 	 * @param {http.ServerResponse} response The document already
 	 *     downloaded from the server
@@ -154,7 +154,7 @@ module.exports = function(_base, _asset, _path, _directory) {
 	/**
 	 * Will perform modifications on downloaded content depending on content
 	 * type
-	 * 
+	 *
 	 * @param {URI} uri Document's location
 	 * @param {http.ServerResponse} response The document already
 	 *     downloaded from the server
@@ -177,7 +177,7 @@ module.exports = function(_base, _asset, _path, _directory) {
 
 	/**
 	 * Will save the modified document in the filesystem
-	 * 
+	 *
 	 * @param {URI} uri Document's location
 	 * @param {string} contentType Document's content type
 	 * @param {string} content Document's content
@@ -189,7 +189,7 @@ module.exports = function(_base, _asset, _path, _directory) {
 		var relative = _path(uri, contentType);
 		var absolute = path.resolve(_directory, relative);
 		var directory = path.dirname(absolute);
-		
+
 		mkdirp(directory, function(err) {
 			if (err) {
 				cb(err);
@@ -205,37 +205,37 @@ module.exports = function(_base, _asset, _path, _directory) {
 			});
 		});
 	};
-	
-	
-	
-	
-	
+
+
+
+
+
 	/**
 	 * Will download `uri' and invoke `cb' with the relevant information
-	 * 
+	 *
 	 * @param {URI} uri The document's location
 	 * @param {callback(err, array of uri)} cb Callback function which will
 	 *     be invoked as soon as the document is saved to the local
 	 *     filesystem.
-	 *     
+	 *
 	 *     While references to assets will be resolved inside this function,
 	 *     references to other document will not and are left in the
 	 *     responsibility of the caller
 	 */
 	return function(uri, cb) {
-		
+
 		/* Check if uri is below base
 		 */
 		uri = uri.normalize();
 		var base_s = _base.toString();
 		var uri_s = uri.toString();
-		
+
 		if (0 !== uri_s.indexOf(base_s)) {
 			cb(new Error('Document at `'+ uri_s +'\' is not below base URI `'+ base_s +'\''));
 			return;
 		}
-		
-		
+
+
 		/* Download, modify and save document. After that notify the
 		 * callback function
 		 */
--- src/Path.js
+++ src/Path.js
@@ -2,23 +2,23 @@
  * Copyright (c) 2014 github/ooxi
  *     https://github.com/ooxi/make-static
  *     violetland@mail.ru
- * 
+ *
  * This software is provided 'as-is', without any express or implied warranty.
  * In no event will the authors be held liable for any damages arising from the
  * use of this software.
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
+ *
  *  1. The origin of this software must not be misrepresented; you must not
  *     claim that you wrote the original software. If you use this software in a
  *     product, an acknowledgment in the product documentation would be
  *     appreciated but is not required.
- * 
+ *
  *  2. Altered source versions must be plainly marked as such, and must not be
  *     misrepresented as being the original software.
- * 
+ *
  *  3. This notice may not be removed or altered from any source distribution.
  */
 var md5 = require('MD5');
@@ -32,7 +32,7 @@ var URI = require('URIjs');
 /**
  * Converts document URIs to relative paths. It is assumed that all documents
  * contain HTML.
- * 
+ *
  * @param {URI} _base Base URI, paths to other URIs should be relative to this
  */
 module.exports = function(_base) {
@@ -41,11 +41,11 @@ module.exports = function(_base) {
 	 * URI -> path cache for faster lookup
 	 */
 	var _cache = {};
-	
-	
-	
-	
-	
+
+
+
+
+
 	/**
 	 * @param {string} url_path URI path
 	 * @return {string} Save filesystem path which looks somehow similar to
@@ -54,30 +54,30 @@ module.exports = function(_base) {
 	var get_save_path = function(url_path) {
 		var parts = url_path.split('/');
 		var os_path = '';
-		
+
 		for (var i = 0; i < parts.length; ++i) {
 			if (i > 0) {
 				os_path += path.sep;
 			}
 			os_path += get_safe_name(parts[i]);
-			
+
 		}
 
 		return os_path;
 	};
-	
+
 	/**
 	 * @param {string} filename Filename to make filesystem safe
 	 * @return {string} Name of file safe to write in filesystem
 	 */
 	var get_safe_name = function(filename) {
-		
+
 		/* An empty part means a directory listing
 		 */
 		if ('' === filename) {
 			return 'index.html';
 		}
-		
+
 		/* Most files should be safe without any modification since we
 		 * use URL encoded filenames. This is great since most unix
 		 * file systems and even NTFS support % as filename character
@@ -85,44 +85,44 @@ module.exports = function(_base) {
 		 */
 		return filename.replace(/[^a-z0-9.%_-]/gi, '_');
 	};
-	
+
 	/**
 	 * @param {string} os_path Base relative save path determined by
 	 *     `get_save_path'
 	 * @param {object or null} query URI query
-	 * 
+	 *
 	 * @warning This is no canonical query hash function so which means
 	 *     `index.php?a=b&c=d' will probably have a different hash than
 	 *     `index.php?c=d&a=b' but I don't care and websites are broken if
 	 *     they offer the same content without using canonical URLs
-	 * 
+	 *
 	 * @return Modifieds path so query hash is included so `list.php?s=asc'
 	 *     will be saved in a different file than `list.php?s=desc'
 	 */
 	var append_query = function(os_path, query) {
-		
+
 		/* No query
 		 */
 		if (0 === Object.keys(query).length) {
 			return os_path;
 		}
 		var query_hash = md5(JSON.stringify(query));
-		
+
 		/* Get file extension, if there is no file extension we simply
 		 * append the hash
 		 */
 		var extension = path.extname(os_path);
-		
+
 		if ('' === extension) {
 			return os_path +'-'+ query_hash;
 		}
 		return os_path.substr(0, os_path.length - extension.length) +'-'+ query_hash + extension;
 	};
-	
-	
-	
-	
-	
+
+
+
+
+
 	/**
 	 * @param {URI} uri Document's location
 	 * @param {string} contentType Document's content type
@@ -130,40 +130,40 @@ module.exports = function(_base) {
 	 *     otherwise false
 	 */
 	return function(uri, contentType) {
-		
+
 		if (!(uri instanceof URI)) {
 			throw new Error('Argument must be an instance of URI but is `'+ uri +'\'');
 		}
 		uri = uri.normalize();
 		var uri_s = uri.toString();
-		
+
 		/* If uri is not below base it should not be made static
 		 */
 		if (0 !== uri_s.indexOf(_base.toString())) {
 			return false;
 		}
-		
+
 		/* Use cache if available
 		 */
 		if (_cache.hasOwnProperty(uri_s)) {
 			return _cache[uri_s];
 		}
-		
+
 		/* First get file name without looking at the query
 		 */
 		var relative = uri.relativeTo(_base);
 		var path = get_save_path(relative.path());
-		
+
 		/* If URI contains a query, append query hash to file name
 		 */
 		path = append_query(path, relative.query(true));
-		
+
 		/* Append HTML file extension
 		 */
 		if (('text/html' === contentType) && !/\.html?$/.test(path)) {
 			path += '.html';
 		}
-		
+
 		/* Cache returned result
 		 */
 		return _cache[uri_s] = path;
